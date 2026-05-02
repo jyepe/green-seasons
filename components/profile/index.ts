f1a58bc0b1021c434c1eea0457423a57be06b687
MOTHERSMILK\yepej <julianyepe94@proton.me>
2026-05-02 11:10:11 -0400

    feat(profile): add barrel export for profile components

---
+++ components/profile/index.ts
@@ -0,0 +1,6 @@
+// components/profile/index.ts
+export { ProfileAvatar } from './ProfileAvatar';
+export { ProfileHeader } from './ProfileHeader';
+export { ProfileRow } from './ProfileRow';
+export { ProfileSection } from './ProfileSection';
+export { ProfileToggleRow } from './ProfileToggleRow';
