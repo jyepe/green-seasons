38a212a7c947303979d03c99901c3a52702c5338
MOTHERSMILK\yepej <julianyepe94@proton.me>
2026-05-04 01:57:38 -0400

    feat(dashboard): add barrel export for components/dashboard

---
+++ components/dashboard/index.ts
@@ -0,0 +1,14 @@
+export { DashboardHeader } from './DashboardHeader';
+export { KpiTile } from './KpiTile';
+export { KpiRow } from './KpiRow';
+export { RecentOrderRow } from './RecentOrderRow';
+export { RecentOrdersCard } from './RecentOrdersCard';
+export { EmptyOrdersInline } from './EmptyOrdersInline';
+export {
+  greetingForHour,
+  previousMonthLabel,
+  aggregateMtd,
+  formatCompactDollars,
+  formatChange,
+} from './utils';
+export type { Mtd } from './utils';
